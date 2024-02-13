ebef502f98e0dfaad02416b1254edaf74cb26088
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-02-13 10:54:07 +0100

    mise à jour

--- Exercices/Corrections/exo_06.py
+++ Exercices/Corrections/exo_06.py
@@ -1,10 +1,9 @@
 epaisseur_papier_mm = 0.1
 epaisseur_cible_mm = 1000 * 400
-
 nombre_de_plis = 0
-epaisseur_actuelle = epaisseur_papier_mm
-while epaisseur_actuelle <= epaisseur_cible_mm:
-    epaisseur_actuelle *= 2
+
+while epaisseur_papier_mm <= epaisseur_cible_mm:
+    epaisseur_papier_mm *= 2
     nombre_de_plis += 1
 
 print(f"Nombre de plis nécessaires: {nombre_de_plis}")
