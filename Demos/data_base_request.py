47d5fbc68e84a2224ab01f86fda2a1441e69af55
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-02-14 17:26:35 +0100

    Ajout demo graph

--- Demos/data_base_request.py
+++ Demos/data_base_request.py
@@ -4,18 +4,18 @@ from psycopg2 import Error
 try:
     # Connect to the PostgreSQL database (replace these with your database details)
     connection = psycopg2.connect(
-        user="your_username",
-        password="your_password",
-        host="your_host",
-        port="your_port",
-        database="your_database"
+        user="postgres",
+        password="pass123",
+        host="127.0.0.1",
+        port="5432",
+        database="postgres"
     )
 
     # Create a cursor object using the cursor() method
     cursor = connection.cursor()
 
     # Define your SELECT query
-    select_query = "SELECT * FROM your_table"
+    select_query = "SELECT * FROM public.utilisateurs;"
 
     # Execute the SELECT query
     cursor.execute(select_query)
