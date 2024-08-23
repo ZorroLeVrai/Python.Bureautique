e106a34b699df72019d8ae14648105fb9fde5e20
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-08-23 13:21:32 +0200

    Ajout d'exemples

---
+++ Exemples/ex_005.2.py
@@ -0,0 +1,3 @@
+temperature_eau = float(input("Entrez la température de l'eau: "))
+eau_liquide = (temperature_eau >= 0) and (temperature_eau < 100)
+print("L'eau est t-elle à l'état liquide:", eau_liquide)
---
+++ Exemples/ex_006.2.py
@@ -0,0 +1,3 @@
+age = float(input("Entrez votre age en année: "))
+majeur = age >= 18
+print("Vous êtes majeur:", majeur)
