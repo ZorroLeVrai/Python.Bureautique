5daa6131630677f5763fc28ef4d0a778554fc26c
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-08-24 07:10:00 +0200

    Ajout d'exemples plus concrets

---
+++ Exemples/ex_017_2.py
@@ -0,0 +1,10 @@
+liste_voiture = []
+liste_non_terminee = True
+while liste_non_terminee:
+    marque_voiture = input("Entrez la marque de la voiture: ")
+    if marque_voiture:
+        liste_voiture.append(marque_voiture)
+    else:
+        liste_non_terminee = False
+
+print(liste_voiture)
