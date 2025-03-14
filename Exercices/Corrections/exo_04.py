d8f246ca725167d11aeb9e877c88d997930dead7
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2025-03-14 17:51:58 +0100

    MAJ des corrigés exercices

--- Exercices/Corrections/exo_04.py
+++ Exercices/Corrections/exo_04.py
@@ -1,10 +1,10 @@
 temperature = float(input("Entrez la température de l'eau: "))
 
 if temperature < 0:
-    etat_eau = "solide"
+    etat_eau = "SOLIDE"
 elif temperature <= 100:
-    etat_eau = "liquide"
+    etat_eau = "LIQUIDE"
 else:
-    etat_eau = "gazeux"
+    etat_eau = "GAZEUX"
 
-print("Etat", etat_eau)
+print("Etat de l'eau:", etat_eau)
