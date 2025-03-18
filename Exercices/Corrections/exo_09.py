dbcf461f7ca9a3a2c7811428504ea20d13efaab5
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2025-03-18 09:45:51 +0100

    Update exo_09.py

--- Exercices/Corrections/exo_09.py
+++ Exercices/Corrections/exo_09.py
@@ -2,8 +2,8 @@ def compter_lettre_a(texte):
     nb_lettres = 0
     for lettre in texte.upper():
         if lettre == "A":
-            nb_lettres += 1
+            nb_lettres += 1  # nb_lettres = nb_lettres + 1
     return nb_lettres
 
-print(compter_lettre_a("abba"))
+print(compter_lettre_a("Abba"))
 print(compter_lettre_a("mixer"))
