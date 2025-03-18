da1f6555fd36247dc8531fde97e92e78e787614d
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2025-03-18 10:41:37 +0100

    Update exo_11bis.py

--- Exercices/Corrections/exo_11bis.py
+++ Exercices/Corrections/exo_11bis.py
@@ -1,8 +1,8 @@
-message_saisie = "Veuillez entrer une note entre 0 et 20 compris (une note négative stoppera la saisie): "
+MESSAGE_SAISIE = "Veuillez entrer une note entre 0 et 20 compris (une note négative stoppera la saisie): "
 
 def saisir_une_note():
     while True:
-        note_str = input(message_saisie)
+        note_str = input(MESSAGE_SAISIE)
         try:
             note = float(note_str)
             if note > 20:
@@ -13,16 +13,21 @@ def saisir_une_note():
             print("Veuillez saisir un nombre entier ou un nombre décimal")
 
 def saisir_notes():
-    liste = []
+    notes = []
     note = 0
     while note >= 0:
         note = saisir_une_note()
         if note >= 0:
-            liste.append(note)
+            notes.append(note)
     
-    return liste
+    return notes
+
 
 liste_notes = saisir_notes()
+if len(liste_notes) == 0:
+    print("Aucune note saisie")
+    exit()
+
 print("Note minimale:", min(liste_notes))
 print("Note maximale:", max(liste_notes))
 print("Note moyenne:", sum(liste_notes) / len(liste_notes))
